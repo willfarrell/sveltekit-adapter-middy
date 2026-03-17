eb597eee363fc102b077081108889411239e5f6b
Jayakishore Roy <jayakishore.r@proton.me>
2026-03-17 13:59:15 -0400

    fix: update default handler to use middy v7 executionMode API
    Signed-off-by: Jayakishore Roy <jayakishore.r@proton.me>

--- handler.js
+++ handler.js
@@ -1,10 +1,11 @@
 // Copyright 2026 will Farrell, and sveltekit-adapter-middy contributors.
 // SPDX-License-Identifier: MIT
 import middy from "@middy/core";
+import { executionModeStreamifyResponse } from "@middy/core/StreamifyResponse";
 import sveltekitHandler from "./sveltekitHandler.js";
 import sveltekitMiddleware from "./sveltekitMiddleware.js";
 
-export const handler = middy({ streamifyResponse: true })
+export const handler = middy({ executionMode: executionModeStreamifyResponse })
 	.use([
 		// other middleware here
 		sveltekitMiddleware(),
